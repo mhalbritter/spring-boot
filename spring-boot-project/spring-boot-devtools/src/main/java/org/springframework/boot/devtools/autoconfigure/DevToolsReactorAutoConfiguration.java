cda49b602845afec4033a032dcba32aa3ba2f3e4
Moritz Halbritter <mkammerer@vmware.com>
2023-01-17 11:18:30 +0100

    Set shutdownQuietPeriod on ReactorResourceFactory to 0

---
+++ spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/autoconfigure/DevToolsReactorAutoConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.devtools.autoconfigure;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.ClientHttpConnectorAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for DevTools-specific Reactor
+ * configuration.
+ *
+ * @author Moritz Halbritter
+ * @since 2.7.8
+ */
+@ConditionalOnClass(ReactorResourceFactory.class)
+@Conditional({ OnEnabledDevToolsCondition.class })
+@AutoConfiguration(after = { ReactiveWebServerFactoryAutoConfiguration.class, RSocketServerAutoConfiguration.class,
+		ClientHttpConnectorAutoConfiguration.class })
+class DevToolsReactorAutoConfiguration {
+
+	@Bean
+	ReactorResourceFactoryBeanPostProcessor reactorResourceFactoryBeanPostProcessor() {
+		return new ReactorResourceFactoryBeanPostProcessor();
+	}
+
+}
---
+++ spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/autoconfigure/ReactorResourceFactoryBeanPostProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.devtools.autoconfigure;
+
+import java.time.Duration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+
+/**
+ * @author Moritz Halbritter
+ */
+class ReactorResourceFactoryBeanPostProcessor implements BeanPostProcessor {
+
+	private static final Log logger = LogFactory.getLog(ReactorResourceFactoryBeanPostProcessor.class);
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof ReactorResourceFactory) {
+			logger.info("Configuring shutdown quiet period");
+			((ReactorResourceFactory) bean).setShutdownQuietPeriod(Duration.ZERO);
+		}
+		return bean;
+	}
+
+}
