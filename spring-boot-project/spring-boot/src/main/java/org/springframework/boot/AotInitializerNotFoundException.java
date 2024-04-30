3c935c9bfb354b74d7a5b839d8b4278deec011db
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-04-30 12:26:52 +0200

    Add exception and failure analyzer for missing AOT initializer

---
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/AotInitializerNotFoundException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot;
+
+/**
+ * Thrown when the AOT initializer couldn't be found.
+ * @author Moritz Halbritter
+ */
+class AotInitializerNotFoundException extends RuntimeException {
+	private final Class<?> mainClass;
+
+	private final String initializerClassName;
+
+	AotInitializerNotFoundException(Class<?> mainClass, String initializerClassName) {
+		this.mainClass = mainClass;
+		this.initializerClassName = initializerClassName;
+	}
+
+	Class<?> getMainClass() {
+		return this.mainClass;
+	}
+
+	String getInitializerClassName() {
+		return this.initializerClassName;
+	}
+}
