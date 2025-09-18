dbd07d86f9b6e40fca6ce827b5d061a0e461d1e3
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-09-18 12:24:38 +0200

    Add nullability annotations to tests in module/spring-boot-data-cassandra

--- module/spring-boot-data-cassandra/src/test/java/org/springframework/boot/data/cassandra/autoconfigure/CassandraReactiveRepositoriesAutoConfigurationTests.java
+++ module/spring-boot-data-cassandra/src/test/java/org/springframework/boot/data/cassandra/autoconfigure/CassandraReactiveRepositoriesAutoConfigurationTests.java
@@ -93,7 +93,9 @@ class CassandraReactiveRepositoriesAutoConfigurationTests {
 
 	private ManagedTypes getManagedTypes(ApplicationContext context) {
 		CassandraMappingContext mappingContext = context.getBean(CassandraMappingContext.class);
-		return (ManagedTypes) ReflectionTestUtils.getField(mappingContext, "managedTypes");
+		Object field = ReflectionTestUtils.getField(mappingContext, "managedTypes");
+		assertThat(field).isNotNull();
+		return (ManagedTypes) field;
 	}
 
 	@Configuration(proxyBeanMethods = false)
