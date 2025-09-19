0f4e099fe4f39f6c6c1e0c54e74c313923eae84a
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-09-19 12:42:28 +0200

    Add nullability annotations to tests in module/spring-boot-data-r2dbc

--- module/spring-boot-data-r2dbc/src/test/java/org/springframework/boot/data/r2dbc/autoconfigure/R2dbcDataAutoConfigurationTests.java
+++ module/spring-boot-data-r2dbc/src/test/java/org/springframework/boot/data/r2dbc/autoconfigure/R2dbcDataAutoConfigurationTests.java
@@ -50,6 +50,7 @@ class R2dbcDataAutoConfigurationTests {
 		this.contextRunner.withUserConfiguration(TestConfiguration.class).run((context) -> {
 			R2dbcMappingContext mappingContext = context.getBean(R2dbcMappingContext.class);
 			ManagedTypes managedTypes = (ManagedTypes) ReflectionTestUtils.getField(mappingContext, "managedTypes");
+			assertThat(managedTypes).isNotNull();
 			assertThat(managedTypes.toList()).containsOnly(City.class);
 		});
 	}
